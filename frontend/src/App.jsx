7d51eeb95a6a10188c8b09adcc433e570636fa4b
praveen1817 <praveen42165@gmail.com>
2026-01-14 01:49:05 +0530

    Added / Routes to Login and fixed Loading Issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate } from 'react-router-dom';
 import Signin from './pages/Signin';
 import Login from './pages/Login';
 import Home from './pages/Home/Home'
@@ -8,14 +8,16 @@ import './App.css'
 
 function App() {
   return (
+    <>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path='/home' element={<Home/>}></Route>
       <Route path="/signin" element={<Signin />} />
       <Route path="/login" element={<Login />} />
       <Route path="/news" element={<News />} />
       <Route path="/analyze" element={<Analyse />} />
-
     </Routes>
+    </>
   );
 }
 
